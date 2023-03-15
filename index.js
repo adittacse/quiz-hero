8123e050b78b90b136409736498b5e6af715df80
Aditta Chakraborty <adittacse@gmail.com>
2023-03-16 02:58:33 +0600

    button click addEventListener fixed

--- index.js
+++ index.js
@@ -60,21 +60,21 @@ const displayQuiz = (data) => {
 
   data.forEach((quiz, i) => {
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
+    <div class="flex items-center">
+      <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+        ${i + 1}
+      </div>
+      <p class="text-gray-800 text-sm">${quiz.quetion}</p>
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
+    <div class="grid grid-cols-2 gap-4 mt-5">
+      ${displayQuizOptions(quiz.options, i)}
+    </div>
+  </div>`;
   });
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
